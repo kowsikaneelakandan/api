f1ea2e5d00961f0f7c01b6d608c7dfc078ad2598
kowsika <kowsikaneelakandan@gmail.com>
2025-01-05 14:11:16 +0530

    Forward path-clg

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,6 +6,7 @@ import jwt from "jsonwebtoken";
 //Sign-Up
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  console.log("signup",req.body)
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
@@ -19,6 +20,7 @@ export const signup = async (req, res, next) => {
 //Sign-In
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
+  console.log("signin",req.body)
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User Not Found!"));
@@ -26,6 +28,7 @@ export const signin = async (req, res, next) => {
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET || 'todo');
     const { password: pass, ...rest } = validUser._doc;
+    console.log("validUse",token)
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
--- index.js
+++ index.js
@@ -39,11 +39,11 @@ app.use("/api/user", userRouter); //api/user
 app.use("/api/auth", authRouter); //api/auth
 app.use("/api/listing", listingRouter); //api/listing
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "/client/dist")));
+// 
+// app.get("*", (req, res) => {
+  // res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 //Errors Handler
 app.use((err, req, res, next) => {
